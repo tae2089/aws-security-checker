ee15a1559567c86b5219c11b07eea89fea9972f6
tae2089 <tae2089002@gmail.com>
2024-09-29 13:22:31 +0900

    add version cmd

--- cmd/version.go
+++ cmd/version.go
@@ -1,3 +1,16 @@
 package cmd
 
+import "github.com/spf13/cobra"
+
 var version = "VERSION"
+
+func GetVersionCmd() *cobra.Command {
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of aws-security-checker",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Printf("aws-security-checker version -  %s\n", version)
+		},
+	}
+	return versionCmd
+}
